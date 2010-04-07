ef0315e3033893cf0eb53ac48e1b8b0999e5dfc8
Magnus Osterlind <dooz@spotify.com>
2010-04-07 19:17:48 +0200

    .

--- celsus/celsus.cpp
+++ celsus/celsus.cpp
@@ -1,6 +1,29 @@
 #include "stdafx.h"
 #include "celsus.hpp"
 
+uint8_t* load_file(const char* filename, const bool zero_terminate, uint32_t* len)
+{
+	const HANDLE file_handle = CreateFileA(filename, GENERIC_READ, FILE_SHARE_READ, NULL, 
+		OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (file_handle == INVALID_HANDLE_VALUE) {
+		return NULL;
+	}
+
+	const uint32_t file_size = GetFileSize(file_handle, NULL);
+	*len = file_size + (zero_terminate ? 1 : 0);
+	uint8_t* buf = new BYTE[*len];
+	DWORD bytes_read = 0;
+	if (!ReadFile(file_handle, buf, file_size, &bytes_read, NULL) || bytes_read != file_size) {
+		return NULL;
+	}
+
+	CloseHandle(file_handle);
+	if (zero_terminate) {
+		buf[file_size] = 0;
+	}
+	return buf;
+}
+
 bool load_file(uint8_t*& buf, uint32_t& len, const char* filename, const bool zero_terminate)
 {
   const HANDLE file_handle = CreateFileA(filename, GENERIC_READ, FILE_SHARE_READ, NULL, 
